c1881a9e5b162ae54dc438fe246291fa7a71b32c
Mostafa Khamis Hassan <69043285+mostafa-khamis@users.noreply.github.com>
2023-11-23 15:21:32 +0200

    Upload Rest Security App

---
+++ restsecurity/src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+@Bean
+public UserDetailsManager userDetailsManager(DataSource dataSource){
+    JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+    jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id,pw,active from members where user_id =?");
+    jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id=?");
+
+    return jdbcUserDetailsManager;
+}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->  configurer.requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                                                            .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                                                            .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                                                            .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                                                            .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+    http.httpBasic(Customizer.withDefaults());
+    http.csrf(csrf -> csrf.disable());
+    return http.build();
+    }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//        UserDetails mostafa = User.builder()
+//                .username("mostafa")
+//                .password("{noop}123")
+//                .roles("EMPLOYEE","MANAGER","ADMIN")
+//                .build();
+//        UserDetails rouqia = User.builder()
+//                .username("rouqia")
+//                .password("{noop}123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//        UserDetails yuri = User.builder()
+//                .username("Yuri")
+//                .password("{noop}123")
+//                .roles("EMPLOYEE")
+//                .build();
+//        return new InMemoryUserDetailsManager(mostafa,rouqia,yuri);
+//    }
+}
